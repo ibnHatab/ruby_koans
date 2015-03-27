7ec5ccb52385cbef3f4a526b255ab1370e9febdb
ibnHatab <vkinzersky@yahoo.com>
2015-03-27 08:27:04 +0000

    score

--- about_scoring_project.rb
+++ about_scoring_project.rb
@@ -30,28 +30,25 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # Your goal is to write the score method.
 
 def score(dice)
+  total = 0
+  return total if dice == []
   
-  sdice = dice.sort
-  
-  
-  puts '>>' + dice.to_s
-
-  val = case dice
-    when []
-      0
-    when [5]
-      50
-    when [1]
-      100
-    else
-      0
+  slice = dice.sort
+  if slice.length >= 3
+    x,y,z = slice
+    if x == y && y == z
+      total += x==1 ? 1000 : x*100
+      slice = slice[3..-1]
+    end
   end
-
-  return val if dice == []
   
-  puts "1>> #{val}"
-  
-  val + score(dice[1..-1])
+  slice.inject(total) do |sum, num|
+    sum + case num
+            when 5;  50
+            when 1;  100
+            else 0
+          end
+  end
 end
 
 class AboutScoringProject < Neo::Koan
